1475beb699622f94d8e6659f7d87876d7e6ee6d6
Tural <tural176@mail.ru>
2024-02-26 10:18:32 +0400

    lesson19

---
+++ Tasks/Extension.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tasks
+{
+   public static class Extension
+    {
+        #region Sentences
+        public static void Sentences(this string input, string sentences)
+        {
+            
+             int count = 0;
+
+            string[] words = sentences.Split(' ');
+
+            foreach (var word in words)
+            {
+
+                foreach (var sameWord in words)
+                {
+                    if (sameWord.ToLower() == word.ToLower())
+                    {
+                        count++;
+                    }
+                }
+
+                if (count > 1)
+                {
+                    Console.WriteLine($"there is {word} word appointed {count} times");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("there is no same words appointed");
+                }
+            }
+        }
+
+        #endregion
+
+        #region SeasonMonth
+        public static Month[] ShowSeasonOfMonth(this Season season)
+        {
+            switch (season)
+            {
+                case Season.Winter:
+
+                    return new Month[] { Month.December, Month.February, Month.January };
+                case Season.Spring:
+
+                    return new Month[] { Month.March, Month.April, Month.May };
+
+                case Season.Summer:
+                    return new Month[] { Month.June, Month.July, Month.August };
+
+                case Season.Autumn:
+                    return new Month[] { Month.September, Month.October, Month.November };
+
+                default:
+                    return null;
+            }
+        }
+        #endregion
+    }    
+}   
---
+++ Tasks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string task = null;
+
+            task.Sentences("Qarabag zeferimiz Qarabag qururumuzdur Qarabag oz yurdumuzdur Qarabag can Azerbaycandir");
+
+            //////////////////////////////////////////////////////////////////////////////////
+
+            Season season = Season.Summer;
+
+            Month[] month = season.ShowSeasonOfMonth();
+
+            foreach (var item in month)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
